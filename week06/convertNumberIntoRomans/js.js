16c48e499bbf68f3c400ff192c705e03c27e4f4d
ncildag <nurullah.cildag@gmail.com>
2020-04-30 11:31:59 +0100

    FETCH-API AND PROMISE

--- week06/convertNumberIntoRomans/js.js
+++ week06/convertNumberIntoRomans/js.js
@@ -1,3 +1,74 @@
+let arabicRoman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
+
+function arabicToRoman(num){
+    let romanized = "";
+    for (var roman in arabicRoman){
+        while(num >= arabicRoman[roman]){
+            romanized += roman;
+            num -= arabicRoman[roman];
+        }
+    }
+    return romanized;
+}
+
+document.write(arabicToRoman(1907));
+
+
+// ================Different Syntax - 1============================
+
+// arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+// roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+// function arabicToRoman(num){
+//     let romanized = "";
+//     for(let i = 0; i < arabic.length; i++){
+//         while(num >= arabic[i]){
+//             romanized += roman[i];
+//             num -= arabic[i];
+//         }
+//     }
+//     return romanized;
+// }
+
+// document.write(arabicToRoman(1907));
+
+
+
+// ================Different Syntax - 2============================
+
+// let arabicRoman = [
+//     {1000: "M"}, {900: "CM"}, {500: "D"}, {400: "CD"}, {100: "C"}, {90: "XC"},
+//     {50: "L"}, {40: "XL"}, {10: "X"}, {9: "IX"}, {5: "V"}, {4: "IV"}, {1: "I"},
+// ];
+
+// function arabicToRoman(num){
+//     let romanized = "";
+//     for(let i = 0; i < arabicRoman.length; i++){
+//         while(num >= Object.keys(arabicRoman[i])){
+//             romanized += Object.values(arabicRoman[i]);
+//             num -= Object.keys(arabicRoman[i]);
+//         }
+//     }
+//     return romanized;
+// }
+
+// document.write(arabicToRoman(1907));
+
+/** 
 let numbers = {
     0:'O',
     1:'I',
@@ -32,6 +103,17 @@ let numbersThree = {
     8 : 'DCCC',
     9 : 'CM'
 }
+let numbersFour = {
+    1 : 'M',
+    2 : 'MM',
+    3 : 'MMM',
+    4 : 'MV',
+    5 : 'V',
+    6 : 'VI',
+    7 : 'VII',
+    8 : 'VIII',
+    9 : 'IX'
+}
 
 function convertRomens(a){
     let number = Number(a); //2
@@ -39,6 +121,7 @@ function convertRomens(a){
     let firstDigits = Number(numberArray[0]);
     let secondDigits = Number(numberArray[1]);
     let thirdDigits = Number(numberArray[2]);
+    let fourthDigits = Number(numberArray[3]);
     
 
     if (a.length == 1){   
@@ -50,9 +133,12 @@ function convertRomens(a){
     if(a.length == 3){
         return `${numbersThree[firstDigits]}${numbersTwo[secondDigits]}${numbers[thirdDigits]}`;
     }
+    if(a.length == 4){
+        return `${numbersFour[firstDigits]}${numbersThree[secondDigits]}${numbersTwo[thirdDigits]}${numbers[fourthDigits]}`;
+    }
 }
-console.log(convertRomens('233'));
+console.log(convertRomens('232'));
 
 
-
+*/
 
---
+++ week06/imageSliderSwiperAPI/fetchAPI/js.js
@@ -0,0 +1,57 @@
+/*let images = [
+    "https://images.unsplash.com/photo-1587583770025-32851bad462e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1587583504540-fee44864486e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1587575494201-11fe74d90d38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+];
+*/
+/** We will use an API to dynamically load images from another website */
+
+//1. declare an empty variable for our images
+let images = [];
+
+//2. fetch the images from external site via API
+fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    .then(response => response.json()) //3. parse the JSON response of API
+    .then(data => {
+        //4. loop through JSON object and get url
+        for(let i=0;i < 10; i++){
+            let obj = data[i];
+            //5. store url in variable
+            images.push(obj.url);
+        }
+    });
+
+let counter = 0;
+let imgTag = document.getElementsByTagName("img")[0];
+let prevBtn = document.getElementsByTagName("button")[0];
+let nextBtn = document.getElementsByTagName("button")[1];
+
+imgTag.src=images[counter];
+
+prevBtn.addEventListener("click",slideToPrev);
+nextBtn.addEventListener("click",slideToNext);
+
+function slideToNext(){
+    //console.log(counter+" >");
+    counter++;
+    if(counter === images.length){
+        counter = 0;
+    }
+    if(counter < images.length){
+        let nextSlide = images[counter];
+        imgTag.src = nextSlide;
+    }
+}
+
+function slideToPrev(){
+    //console.log(counter+" <");
+    counter--;    
+    if(counter < 0){
+        counter = images.length-1;
+    }
+    if(counter >= 0){
+        let prevSlide = images[counter];
+        imgTag.src = prevSlide;
+    }
+}
