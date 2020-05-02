97377d5714b9148728b7635b4d1705e2d7634c97
ncildag <nurullah.cildag@gmail.com>
2020-05-02 14:13:48 +0100

    week06googlemapsapi

---
+++ week06/mapsGoogle/js.js
@@ -0,0 +1,14 @@
+      var map;
+
+      function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+              center: {
+                  lat: 	47.3666687, 
+                  lng: 8.5500002
+              },
+              zoom: 16
+          });
+      }
+
+
+      
