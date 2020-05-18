50c75bdaf37d85b0ef3413934e2f9f1cef969381
ncildag <nurullah.cildag@gmail.com>
2020-05-18 21:11:59 +0100

    monday changing

--- week08/Ismet's Secret Notebook/client.js
+++ week08/Ismet's Secret Notebook/client.js
@@ -1,25 +1,54 @@
-class Client{
-    constructor(){
-        this.clientsList=clientsList;
-        this.cleanerPart=cleanerPart;
-    }
-    bringClientToRightSide(a){
-        let storageName = JSON.parse(localStorage.getItem('newClientsList')) ;
-       
-        console.log(storageName);
-        console.log(storageName);
-        storageName.forEach( k => {
-            if (k == nick) {
-                return this.cleanerPart.innerHTML = `<li>Name : ${element} </li>
-                                                        <li>Adress : ${clientsList[0].information.adress}</li>
-                                                        <li>Country : ${clientsList[0].information.country}</li>
-                                                        <button>Clean It</button>`; 
-            }});
-    }
-    updateList(){
-       
-    }
+class Client {
+  constructor() {
+    this.clientsList = clientsList;
+    this.cleanerPart = cleanerPart;
+  }
 
+  bringClientToRightSide(index) {
+    document.querySelector("#cleanIt").innerHTML = null;
+
+    console.log("---------------index", index);
+
+    let newClientsList = JSON.parse(localStorage.getItem("newClientsList"));
+
+    let clientName = document.createElement("li");
+    let clientAddress = document.createElement("li");
+    let clientCountry = document.createElement("li");
+    let clean = document.createElement("button");
+    document.querySelector("#cleanIt").appendChild(clientName);
+    document.querySelector("#cleanIt").appendChild(clientAddress);
+    document.querySelector("#cleanIt").appendChild(clientCountry);
+    document.querySelector("#cleanIt").appendChild(clean);
+
+    clean.addEventListener("click", () => {
+      newClientsList.forEach((client, index) => {
+        const selectedClientName = document.querySelector("#client-name").innerText;
+
+        if(selectedClientName === client.name) {
+          newClientsList.splice(index, 1);
+          localStorage.setItem("newClientsList", JSON.stringify(newClientsList));
+          document.querySelector("#cleanIt").innerHTML = null;
+        }
+      })
+    });
+
+    newClientsList.forEach((client, i) => {
+      if (i === index) {
+        clientName.innerHTML = `<span>Name:</span> <span id="client-name">${client.name}</span>`;
+        clientAddress.innerText = `Address: ${client.information.adress}`;
+        clientCountry.innerText = `Country: ${client.information.country}`;
+        clean.innerText = `Clean`;
+      }
+    });
+  }
+
+  updateList(clientsNameParamether) {
+    let tmp = " ";
+    for (let i = 0; i < storageName.length; i++) {
+      if (storageName[i].name == clientsNameParamether) {
+        storageName[i].name += tmp;
+        return (ulUpdatedList.innerHTML = `<li>${tmp}</li>`);
+      }
+    }
+  }
 }
-
-
--- week08/Ismet's Secret Notebook/model.js
+++ week08/Ismet's Secret Notebook/model.js
@@ -6,78 +6,86 @@
  * If we click this an item inside order list this should be finished we should define this by highlighting.
  *
  */
-let clientsList = [{
-        name: 'nick',
-        information: {
-            adress: 'Zurichstrasse 21',
-            country: 'switzerland'
-        }
+let clientsList = [
+  {
+    name: "nick",
+    information: {
+      adress: "Zurichstrasse 21",
+      country: "switzerland",
     },
-    {
-        name: 'leo',
-        information: {
-            adress: 'Zurichstrasse 32',
-            country: 'netherlands'
-        }
+  },
+  {
+    name: "leo",
+    information: {
+      adress: "Zurichstrasse 32",
+      country: "netherlands",
     },
-    {
-        name: 'fabianne',
-        information: {
-            adress: 'Zurichstrasse 75',
-            country: 'greece'
-        }
+  },
+  {
+    name: "fabianne",
+    information: {
+      adress: "Zurichstrasse 75",
+      country: "greece",
     },
-    {
-        name: 'christian',
-        information: {
-            adress: 'Zurichstrasse 84',
-            country: 'deutschland'
-        }
-    }
+  },
+  {
+    name: "christian",
+    information: {
+      adress: "Zurichstrasse 84",
+      country: "deutschland",
+    },
+  },
 ];
 
-let ulDomForClientsList = document.querySelector('#myClientsLits');
-let cleanerPart = document.querySelector('#cleanIt');
+localStorage.setItem("newClientsList", JSON.stringify(clientsList));
 
-
+let ulDomForClientsList = document.querySelector("#myClientsLits");
+let ulUpdatedList = document.querySelector("#updatedList");
+let cleanerPart = document.querySelector("#cleanIt");
+let clientData = JSON.parse(localStorage.getItem("newClientsList"));
 
 class Company {
-    constructor() {
-        this.clientsList = clientsList;
-        this.ulDomForClientsList = ulDomForClientsList;
-    }
-    createList() {
-        let count = 0;
-        let str = " ";
-        clientsList.forEach(data => {
-            count += 1;
-            str += `<li id="${data.name}" onclick="client.bringClientToRightSide(${data.name})">${data.name}</li>`;
-            this.ulDomForClientsList.innerHTML = str;
-        });
-    }
-    addEventListenerToClearElement(){
-        // clienti kaydetmek icin event listener ekler.
-        return document.querySelector('#addNewCompany').addEventListener('click', this.saveNewCompany);//function(){
-    }
-    saveNewCompany(){
-        const newClientName = document.querySelector('#newClientName').value;
-        const newClientAdress = document.querySelector('#newClientAdress').value;
-        const newClientCountry = document.querySelector('#newClientAdress').value;
-        console.log(newClientName);
+  constructor() {
+    this.clientsList = clientsList;
+    this.ulDomForClientsList = ulDomForClientsList;
+  }
+  createList() {
+    let count = 0;
+    let str = " ";
+    clientData.forEach((client, index) => {
+      count += 1;
+      str += `<li id="${client.name}" onclick="client.bringClientToRightSide(${index})">${client.name}</li>`;
+      this.ulDomForClientsList.innerHTML = str;
+    });
+  }
+  addEventListenerToClearElement() {
+    // clienti kaydetmek icin event listener ekler.
+    return document
+      .querySelector("#addNewCompany")
+      .addEventListener("click", this.saveNewCompany); //function(){
+  }
+  saveNewCompany() {
+    const newClientName = document.querySelector("#newClientName").value;
+    const newClientAdress = document.querySelector("#newClientAdress").value;
+    const newClientCountry = document.querySelector("#newClientAdress").value;
 
-        return localStorage.setItem('newClientsList', JSON.stringify({
-            ...clientsList,
-            name: newClientName,
-            information: {
-            adress: newClientAdress,
-            country: newClientCountry
-        }}));
-    }
+    let newClientsList = [
+      ...clientsList,
+      {
+        name: newClientName,
+        information: {
+          adress: newClientAdress,
+          country: newClientCountry,
+        },
+      },
+    ];
+
+    localStorage.setItem("newClientsList", JSON.stringify(newClientsList));
+  }
 }
 
 let ismet = new Company();
 ismet.createList();
 ismet.addEventListenerToClearElement();
 let client = new Client();
-//client.addEventListenerToBringElement();
-
+client.updateList();
